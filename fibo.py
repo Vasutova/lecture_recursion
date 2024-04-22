db63386c3d33e5d9f49087c9d24c72e7cc1c74c8
Vasutova <terezievasutova@seznam.cz>
2024-04-22 12:42:07 +0200

    .

--- fibo.py
+++ fibo.py
@@ -1,6 +1,22 @@
 
+
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+    #if n <= 1:
+     #   return n
+
+
+
+
 def main():
-    pass
+    n = int(input("Zadej počet členů posloupnosti: "))
+    seq = [recursive_nth_fibo(num) for num in range(n + 1)]
+    print(seq)
 
 
 if __name__ == "__main__":
